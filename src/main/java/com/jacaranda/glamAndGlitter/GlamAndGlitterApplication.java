3e8484b71941fc6462475e64fed7a4b2cafe7c4e
FranJRG <framirezgarcia0@gmail.com>
2024-09-23 14:01:37 +0200

    creando proyecto

---
+++ src/main/java/com/jacaranda/glamAndGlitter/GlamAndGlitterApplication.java
@@ -0,0 +1,13 @@
+package com.jacaranda.glamAndGlitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlamAndGlitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlamAndGlitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacaranda/glamAndGlitter/GlamAndGlitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacaranda.glamAndGlitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlamAndGlitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
